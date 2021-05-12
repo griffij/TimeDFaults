4c3825d22d4d63e4db2dfecdcbd9096ec3412142
Geoscience Australia <jono.d.griffin@gmail.com>
2021-05-13 09:30:07 +1200

    Code cleanup

--- models/fit_invgauss_sliprate_ksamples_std_params.R
+++ models/fit_invgauss_sliprate_ksamples_std_params.R
@@ -76,144 +76,144 @@ for (datalist in datalists){
     figure_filename = paste0('plots/invgauss_fit_sliprate_std_param_', j, '.pdf')
     pdf(figure_filename)
 
-for (i in 1:nrow(datalist)){
-    data = list(cbind(NA, datalist[i,], NA))#[1,]
-    data = data[[1]]
-    print('data')
-    print(data)
-    print(typeof(data))
-    print(data[4])
-    print(length(datalist)-1)
-    sl = (length(data) - 1)[1]
-    print(sl)
-##    censorLimitVec = cbind(abs(data[2]), 1, 1, 1, 50000-abs(data[sl]))
-    # Most recent event in year before present defines minimum length of
-    # present open interval
-    MRE = abs(data[sl])
-    print('MRE')
-    print(MRE)
-    print(length(data))
-    censorLimitVec = rep(1,length(data)-1)
-    censorLimitVec[1] = 50000-abs(data[2])
-    censorLimitVec[length(censorLimitVec)] = MRE
-    print("censorLimitVec")      
-    print(censorLimitVec)
-#    censorLimitVec = cbind(50000-abs(data[2]), 1, 1, 1, MRE) # Hard coded old version
-    # Convert data to inter-event times
-    m = data.matrix(data)
-    inter_event_m = t(abs(diff(t(m)))) # Transpose, take difference and transpose back
-    isCensored = (inter_event_m < 0)
-    isCensored[1] = TRUE
-    isCensored[length(isCensored)] = TRUE
-    Y = inter_event_m
-    print("Y")
-    print(Y)
-    Y = Y[1,]
-    print(Y)
-    V = throws[1,]
-    T = slip_times[1,]
-    N <- length(Y)
-    M = length(V)
-    isCensored = as.numeric(isCensored)
-    Y[1] = censorLimitVec[1]
-    print(length(Y))
-    print("censorLimitVec[length(Y)]")
-    print(censorLimitVec[length(Y)])
-    Y[length(Y)] = censorLimitVec[length(Y)]
-    print(Y)
-    print('T')
-    print(T)
-    print(V)
-    censorLimitVec = as.numeric(censorLimitVec)
-    print('censorLimitVec')
-    print(censorLimitVec)
-    print(isCensored)
-    print(isSlipCensored)
-    # Lets deal with V and T as uncertain observations
-    V_obs = V
-    T_obs = T
-    # Define data
-    sim.data.jags <- list("Y", "N"
-    		  ,"V_obs", "V_tau", "T_obs", "T_tau"
-    		  ,"M", "isSlipCensored"
-		  ,"censorLimitVec", "isCensored"
-#		  "hf_times"
-		  )
-
-    # Define the parameters whose posterior distributions we want to calculate
-    bayes.mod.params <- c("lambda", "mu", "alpha", "n_events"
-    		     ,"V", "T", "Y", "y[1]", "V_sum", "T_sum", "V_obs", "T_obs"
-		     ) 
-    lambdaInit = 1.0
-    muInit = 1000 # Rough estimate of mean(Y)
-
-    # Define starting values
-    bayes.mod.inits <- function(){list("mu"=muInit, "lambda"=lambdaInit)}
-
-    # The model
-    bayes.mod.fit <- jags(data = sim.data.jags, inits = bayes.mod.inits,
-    	      parameters.to.save = bayes.mod.params, n.chains = 3,
-	      n.iter = 1500, n.burnin = 1000, model.file = 'invgauss_sliprate_std_param.jags')
-	      
-    print(bayes.mod.fit)
-
-    # Convert to an MCMC object
-    bayes.mod.fit.mcmc <- as.mcmc(bayes.mod.fit)
-    # Add to list of all MCMC objects
-    # Get all three chains
-    mcmclist[index] = bayes.mod.fit.mcmc[1]
-    mcmclist[index+1] = bayes.mod.fit.mcmc[2]
-    mcmclist[index+2] = bayes.mod.fit.mcmc[3]
-    index = index+3
-    summary(bayes.mod.fit.mcmc)
-    }
-    
-# Convert all MCMC models into one MCMC object
-bayes.mcmc.combined = as.mcmc.list(mcmclist) # Use this to plot all individual chains
-bayes.mcmc.combinedlist = as.mcmc.list(mcmclist)
-summary(bayes.mcmc.combined) 
-
-# Now plot combined results
-print(xyplot(bayes.mcmc.combined, layout=c(2,2), aspect="fill"))
-# Density plot
-print(densityplot(bayes.mcmc.combined, layout=c(2,2), aspect="fill"))
-#Auto-correlation plot
-#autocorr.plot(bayes.mcmc.combined)
-# Do separate density plot of all combined
-bayes.mcmc.combined = combine.mcmc(mcmclist)
-print(densityplot(bayes.mcmc.combined, layout=c(2,2), aspect="fill"))
-
-# Do some 2D plots
-posterior <- as.array(bayes.mcmc.combined)
-dim(posterior)
-print(mcmc_scatter(posterior, pars = c("mu", "alpha"),
-			size = 1.5, alpha = 0.5))
-h = mcmc_hex(posterior, pars = c("mu", "alpha"))
-h = h + geom_hex(aes_(color = ~ scales::rescale(..density..)))
-h + scale_color_gradientn("Density", colors = unlist(color_scheme_get()),
-  breaks = c(.1, .9), labels = c("low", "high"))
-df_post = do.call(rbind.data.frame, bayes.mcmc.combinedlist)
-
-print(ggplot(df_post, aes(x=mu, y=alpha)) +
-	     stat_density_2d(aes(fill = ..level..), geom = "polygon") +
-	     xlab(expression(mu)) +
-	     ylab(expression(alpha)))
-
-# Dump data to file
-filename = paste0('outputs/df_posterior_', j, '.csv')
-print(filename)
-write.csv(df_post, filename, row.names=FALSE)
-print(typeof(df_post$mu))
-if (j==1){
-   mu_list = df_post$mu
-   alpha_list = df_post$alpha
-}else{
-   mu_list = c(mu_list, df_post$mu)
-   alpha_list[j] = c(alpha_list, df_post$alpha)
-   }
-print("dev.off()")
-dev.off() 
-j = j+1
+	for (i in 1:nrow(datalist)){
+	    data = list(cbind(NA, datalist[i,], NA))#[1,]
+	    data = data[[1]]
+	    print('data')
+	    print(data)
+	    print(typeof(data))
+	    print(data[4])
+	    print(length(datalist)-1)
+	    sl = (length(data) - 1)[1]
+	    print(sl)
+	##    censorLimitVec = cbind(abs(data[2]), 1, 1, 1, 50000-abs(data[sl]))
+	    # Most recent event in year before present defines minimum length of
+	    # present open interval
+	    MRE = abs(data[sl])
+	    print('MRE')
+	    print(MRE)
+	    print(length(data))
+	    censorLimitVec = rep(1,length(data)-1)
+	    censorLimitVec[1] = 50000-abs(data[2])
+	    censorLimitVec[length(censorLimitVec)] = MRE
+	    print("censorLimitVec")      
+	    print(censorLimitVec)
+	#    censorLimitVec = cbind(50000-abs(data[2]), 1, 1, 1, MRE) # Hard coded old version
+	    # Convert data to inter-event times
+	    m = data.matrix(data)
+	    inter_event_m = t(abs(diff(t(m)))) # Transpose, take difference and transpose back
+	    isCensored = (inter_event_m < 0)
+	    isCensored[1] = TRUE
+	    isCensored[length(isCensored)] = TRUE
+	    Y = inter_event_m
+	    print("Y")
+	    print(Y)
+	    Y = Y[1,]
+	    print(Y)
+	    V = throws[1,]
+	    T = slip_times[1,]
+	    N <- length(Y)
+	    M = length(V)
+	    isCensored = as.numeric(isCensored)
+	    Y[1] = censorLimitVec[1]
+	    print(length(Y))
+	    print("censorLimitVec[length(Y)]")
+	    print(censorLimitVec[length(Y)])
+	    Y[length(Y)] = censorLimitVec[length(Y)]
+	    print(Y)
+	    print('T')
+	    print(T)
+	    print(V)
+	    censorLimitVec = as.numeric(censorLimitVec)
+	    print('censorLimitVec')
+	    print(censorLimitVec)
+	    print(isCensored)
+	    print(isSlipCensored)
+	    # Lets deal with V and T as uncertain observations
+	    V_obs = V
+	    T_obs = T
+	    # Define data
+	    sim.data.jags <- list("Y", "N"
+	    		  ,"V_obs", "V_tau", "T_obs", "T_tau"
+	    		  ,"M", "isSlipCensored"
+			  ,"censorLimitVec", "isCensored"
+	#		  "hf_times"
+			  )
+	
+	    # Define the parameters whose posterior distributions we want to calculate
+	    bayes.mod.params <- c("lambda", "mu", "alpha", "n_events"
+	    		     ,"V", "T", "Y", "y[1]", "V_sum", "T_sum", "V_obs", "T_obs"
+			     ) 
+	    lambdaInit = 1.0
+	    muInit = 1000 # Rough estimate of mean(Y)
+	
+	    # Define starting values
+	    bayes.mod.inits <- function(){list("mu"=muInit, "lambda"=lambdaInit)}
+	
+	    # The model
+	    bayes.mod.fit <- jags(data = sim.data.jags, inits = bayes.mod.inits,
+	    	      parameters.to.save = bayes.mod.params, n.chains = 3,
+		      n.iter = 1500, n.burnin = 1000, model.file = 'invgauss_sliprate_std_param.jags')
+		      
+	    print(bayes.mod.fit)
+	
+	    # Convert to an MCMC object
+	    bayes.mod.fit.mcmc <- as.mcmc(bayes.mod.fit)
+	    # Add to list of all MCMC objects
+	    # Get all three chains
+	    mcmclist[index] = bayes.mod.fit.mcmc[1]
+	    mcmclist[index+1] = bayes.mod.fit.mcmc[2]
+	    mcmclist[index+2] = bayes.mod.fit.mcmc[3]
+	    index = index+3
+	    summary(bayes.mod.fit.mcmc)
+	    }
+	    
+	# Convert all MCMC models into one MCMC object
+	bayes.mcmc.combined = as.mcmc.list(mcmclist) # Use this to plot all individual chains
+	bayes.mcmc.combinedlist = as.mcmc.list(mcmclist)
+	summary(bayes.mcmc.combined) 
+	
+	# Now plot combined results
+	print(xyplot(bayes.mcmc.combined, layout=c(2,2), aspect="fill"))
+	# Density plot
+	print(densityplot(bayes.mcmc.combined, layout=c(2,2), aspect="fill"))
+	#Auto-correlation plot
+	#autocorr.plot(bayes.mcmc.combined)
+	# Do separate density plot of all combined
+	bayes.mcmc.combined = combine.mcmc(mcmclist)
+	print(densityplot(bayes.mcmc.combined, layout=c(2,2), aspect="fill"))
+	
+	# Do some 2D plots
+	posterior <- as.array(bayes.mcmc.combined)
+	dim(posterior)
+	print(mcmc_scatter(posterior, pars = c("mu", "alpha"),
+				size = 1.5, alpha = 0.5))
+	h = mcmc_hex(posterior, pars = c("mu", "alpha"))
+	h = h + geom_hex(aes_(color = ~ scales::rescale(..density..)))
+	h + scale_color_gradientn("Density", colors = unlist(color_scheme_get()),
+	  breaks = c(.1, .9), labels = c("low", "high"))
+	df_post = do.call(rbind.data.frame, bayes.mcmc.combinedlist)
+	
+	print(ggplot(df_post, aes(x=mu, y=alpha)) +
+		     stat_density_2d(aes(fill = ..level..), geom = "polygon") +
+		     xlab(expression(mu)) +
+		     ylab(expression(alpha)))
+	
+	# Dump data to file
+	filename = paste0('outputs/df_posterior_', j, '.csv')
+	print(filename)
+	write.csv(df_post, filename, row.names=FALSE)
+	print(typeof(df_post$mu))
+	if (j==1){
+	   mu_list = df_post$mu
+	   alpha_list = df_post$alpha
+	}else{
+	   mu_list = c(mu_list, df_post$mu)
+	   alpha_list[j] = c(alpha_list, df_post$alpha)
+	   }
+	print("dev.off()")
+	dev.off() 
+	j = j+1
 }
 
 png(file='plots/invgauss_fit_sliprate_std_param.png', units="in", width=5, height=5, res=300)
