f7de1b79c7161e13f15f7dd9b6f5b360d36c2f24
Geoscience Australia <jono.d.griffin@gmail.com>
2021-05-20 11:52:48 +1200

    Adding and updating datasets

--- python/build_chronologies.py
+++ python/build_chronologies.py
@@ -3,9 +3,14 @@
 
 from QuakeRates.dataman.parse_params import parse_param_file, get_event_sets
 
-paramfiles = ['../params/Dunstan4event_VanDissen2007_simple.txt',
-              '../params/Dunstan5event_VanDissen2007_simple.txt',
-              '../params/Dunstan6event_VanDissen2007_simple.txt']
+#paramfiles = ['../params/Dunstan4event_VanDissen2007_simple.txt',
+#              '../params/Dunstan5event_VanDissen2007_simple.txt',
+#              '../params/Dunstan6event_VanDissen2007_simple.txt']
+#paramfiles = ['../params/Dunstan6eventOxcal_devonshire.csv',
+#              '../params/Dunstan5eventOxcal_devonshire.csv',
+#              '../params/Dunstan5eventOxcalv2_devonshire.csv',
+#              '../params/Dunstan4eventOxcal_devonshire.csv']
+paramfiles = ['../params/Hyde4event_lugcreek.txt']
 # Number of sample chronologies to generate
 n_samples = 10
 
@@ -13,7 +18,7 @@ names, event_sets, event_certainties, num_events, \
     tect_regions, fault_styles = get_event_sets(paramfiles, ['all'], ['all'], 1)
 print(names)
 for i, event_set in enumerate(event_sets):
-    event_set.gen_chronologies(n_samples, observation_end=2020, min_separation=1)
+    event_set.gen_chronologies(n_samples, observation_end=2020, min_separation=500)
     chron_filename = '../data/chronologies/' + event_set.name + \
         '_%i_chronologies.csv' % n_samples
     event_set.write_chronology(chron_filename)
