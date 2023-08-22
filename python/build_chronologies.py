655d9e75b463ae797ccbd8b1d57138b34be60449
Geoscience Australia <jono.d.griffin@gmail.com>
2023-08-22 11:03:50 +1000

    Adding flag for converting OxCal models into years BP for generating chronologies

--- python/build_chronologies.py
+++ python/build_chronologies.py
@@ -12,16 +12,19 @@ from glob import glob
 #              '../params/Dunstan4eventOxcal_devonshire.csv']
 #paramfiles = ['../params/Hyde3event_lugcreek.txt',
 #              '../params/Hyde4event_lugcreek.txt']
-paramfiles = ['../params/Hyde4event_lugcreek_v3.txt']
+#paramfiles = ['../params/Hyde4event_lugcreek_v3.txt']
+paramfiles = ['../params/Hyde_combined_OSL_CRN.txt']
 # Number of sample chronologies to generate
+current_year = 2023 # Offst for OxCal models defined in AD/BC
 n_samples = 10000
 
 names, event_sets, event_certainties, num_events, \
-    tect_regions, fault_styles = get_event_sets(paramfiles, ['all'], ['all'], 1)
+    tect_regions, fault_styles = get_event_sets(paramfiles, ['all'], ['all'], 1,
+                                                current_year=current_year)
 print(names)
 for i, event_set in enumerate(event_sets):
-    event_set.gen_chronologies(n_samples, observation_end=2020, min_separation=1)
-    chron_filename = '../data/chronologies_all/' + event_set.name + \
+    event_set.gen_chronologies(n_samples, observation_end=current_year, min_separation=1)
+    chron_filename = '../data/chronologies/' + event_set.name + \
         '_%i_chronologies.csv' % n_samples
     event_set.write_chronology(chron_filename)
     
